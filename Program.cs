a024f00c697151c3082d983a2c9a5fd2a38730cd
unknown <americ0nas11@gmail.com>
2026-02-26 01:50:03 +0200

    Clean initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using botas.commands;
+using botas.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Threading.Tasks;
+
+namespace botas
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { jsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<Utility>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ commands/Utility.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace botas.commands
+{
+    internal class Utility : BaseCommandModule
+    {
+        [Command("komandos")]
+        public async Task Komandos(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Komandų sąrašas:" +
+                " .koks");
+        }
+    }
+}
